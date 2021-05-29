0f60c917cea5613b459f8c690c084839bd191725
Billy-96 <biloldinhamdamov2003@gmail.com>
2021-05-29 15:43:06 +0600

    Добавил Авторизацию

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.notes
 
+import android.content.Intent
 import com.example.notes.Fragments.AddingFragment
 import com.example.notes.Fragments.FragmentMain
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +30,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var toggle: ActionBarDrawerToggle
     lateinit var nav: NavigationView
     var db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    lateinit var googleSignInClient: GoogleSignInClient
+    lateinit var auth: FirebaseAuth
+    val RC_SIGN_IN = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +44,11 @@ class MainActivity : AppCompatActivity() {
 
         nav = findViewById(R.id.navigation)
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
         toggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
         drawer.addDrawerListener(toggle)
         toggle.syncState()
@@ -39,12 +59,62 @@ class MainActivity : AppCompatActivity() {
                 R.id.deleted -> Toast.makeText(this, "Deleted Notes", Toast.LENGTH_SHORT).show()
                 R.id.profile -> Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
                 R.id.exit -> System.exit(0)
+                R.id.log_in -> {
+                    googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+                    auth = FirebaseAuth.getInstance()
+                    signIn()
+                }
             }
             true
         }
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
             setFragment(AddingFragment())
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d("MyTag", "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Log.w("MyTag", "Google sign in failed", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("nag", "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Log.w("nag", "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            val view = nav.getHeaderView(0)
+            findViewById<TextView>(R.id.user_name).text = user.displayName
+            findViewById<TextView>(R.id.user_email).text = user.email
+            Glide.with(this).load(user.photoUrl).into(findViewById(R.id.user_icon))
         }
     }
 
